619816bde46c76d28b7468a0c1ea4b64a5fc938e
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-02 17:29:16 +0900

    feat(domain): CourseLevelMissionInput 클래스 추가

---
+++ src/main/java/pairmatching/domain/CourseLevelMissionInput.java
@@ -0,0 +1,25 @@
+package pairmatching.domain;
+
+public class CourseLevelMissionInput {
+    private final Course course;
+    private final Level level;
+    private final Mission mission;
+
+    public CourseLevelMissionInput(Course course, Level level, Mission mission) {
+        this.course = course;
+        this.level = level;
+        this.mission = mission;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public Mission getMission() {
+        return mission;
+    }
+}
