6705223173d677f7f3843214828bf258ceec7f00
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-02 16:28:22 +0900

    feat(domain): MatchingInfo에 MISSION enum 클래스로 변환

--- src/main/java/pairmatching/domain/MatchingInfo.java
+++ src/main/java/pairmatching/domain/MatchingInfo.java
@@ -5,28 +5,23 @@ import java.util.List;
 public class MatchingInfo {
     private Course course;
     private Level level;
-    private String missionName;
+    private Mission mission;
     private List<Pair> pairs;
 
-    public MatchingInfo(Course course, Level level, String missionName, List<Pair> pairs) {
+    public MatchingInfo(Course course, Level level, Mission mission, List<Pair> pairs) {
         this.course = course;
         this.level = level;
-        this.missionName = missionName;
+        this.mission = mission;
         this.pairs = pairs;
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Course: ").append(course.getName()).append("\n");
-        sb.append("Level: ").append(level.getName()).append("\n");
-        sb.append("Mission: ").append(missionName).append("\n");
-
-        sb.append("Pairs:\n");
-        for (Pair pair : pairs) {
-            sb.append(pair).append("\n");
-        }
-
-        return sb.toString();
+        return "MatchingInfo{" +
+                "course=" + course +
+                "\nlevel=" + level +
+                "\nmission=" + mission +
+                "\npairs=" + pairs +
+                '}';
     }
 }
