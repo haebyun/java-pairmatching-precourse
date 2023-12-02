3c7539089f93623d313dcac3dfe7aeb702caf7a7
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-02 15:33:47 +0900

    feat(domain): CrewRepository 추가
    - 파일 로드기능
    - 크루 셔플 기능

---
+++ src/main/java/pairmatching/domain/CrewRepository.java
@@ -0,0 +1,43 @@
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+public class CrewRepository {
+    private List<Crew> crews = new ArrayList<>();
+
+    public void loadCrewsFromFiles(String backendFilePath, String frontendFilePath) {
+        loadCrewsFromFile(backendFilePath, Course.BACKEND);
+        loadCrewsFromFile(frontendFilePath, Course.FRONTEND);
+    }
+
+    public void loadCrewsFromFile(String filePath, Course course) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            for (String name : lines) {
+                crews.add(new Crew(course, name));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Crew> getCrews() {
+        return crews;
+    }
+
+    public void shuffleCrews(List<Crew> selectedCrews) {
+        this.crews = Randoms.shuffle(selectedCrews);
+    }
+    public List<Crew> getCrewsByCourse(Course course) {
+        return crews.stream()
+                .filter(crew -> crew.getCourse() == course)
+                .collect(Collectors.toList());
+    }
+}
