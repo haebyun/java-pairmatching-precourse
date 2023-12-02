8fac5353efc77121d17b8abb439039c413c14ff8
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-02 15:34:48 +0900

    feat(): 코스별 랜덤 짝 기능 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,24 @@
 package pairmatching;
 
+import pairmatching.domain.*;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        CrewRepository repository = new CrewRepository();
+        // 크루 목록 로드
+        repository.loadCrewsFromFiles("src/main/resources/backend-crew.md", "src/main/resources/frontend-crew.md");
+
+        Course selectedCourse = Course.FRONTEND;
+
+        List<Crew> selectedCrews = repository.getCrewsByCourse(selectedCourse);
+        repository.shuffleCrews(selectedCrews);
+
+        PairMatcher pairMatcher = new PairMatcher();
+        List<Pair> pairs = pairMatcher.matchPairs(repository.getCrews());
+
+        // 페어 출력 로직
+        pairs.forEach(System.out::println);
     }
 }
