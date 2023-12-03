d8aa6f15faeea57040a3d9c1f5229e3e0a83c80f
haebyun <bhk4392@gmail.com>
2023-12-03 16:28:03 +0900

    fix(InputValidator): 잘못된 입력에 대한 예외처리 추가

--- src/main/java/pairmatching/model/validation/InputValidator.java
+++ src/main/java/pairmatching/model/validation/InputValidator.java
@@ -7,18 +7,19 @@ import pairmatching.model.constants.Level;
 
 public class InputValidator {
     public static void validateFeatureOption(String input) {
-        if(!Pattern.matches("^[1-3]$", input)) {
+        if (!Pattern.matches("^[1-3]$", input)) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
     }
 
     public static void validateRematchingOption(String option) {
-        if(!option.equals("네") && !option.equals("아니오")) {
+        if (!option.equals("네") && !option.equals("아니오")) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
     }
 
     public static void validateSequenceOfOptions(List<String> options) {
+        validateInvalidOptionsSize(options);
         String course = options.get(0);
         String level = options.get(1);
         String mission = options.get(2);
@@ -27,20 +28,26 @@ public class InputValidator {
         validateInvalidMission(mission);
     }
 
+    private static void validateInvalidOptionsSize(List<String> options) {
+        if (options.size() != 2) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
     private static void validateInvalidCourse(String course) {
-        if(!Course.isCourse(course)) {
+        if (!Course.isCourse(course)) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
     }
 
     private static void validateInvalidLevel(String level) {
-        if(!Level.isActualLevel(level)) {
+        if (!Level.isActualLevel(level)) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
     }
 
     private static void validateInvalidMission(String mission) {
-        if(!Level.isActualMission(mission)) {
+        if (!Level.isActualMission(mission)) {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
     }
