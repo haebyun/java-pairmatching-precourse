05e2ce4a491ebf82dcc23553dfe04acdfe293d5a
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-07 16:00:51 +0900

    feat(domain): Mission 클래스 필드 주석 삭제

--- src/main/java/pairmatching/domain/Mission.java
+++ src/main/java/pairmatching/domain/Mission.java
@@ -1,17 +1,14 @@
 package pairmatching.domain;
 
 public enum Mission {
-    // LEVEL 1 Missions
     CAR_RACING(Level.LEVEL1, "자동차경주"),
     LOTTO(Level.LEVEL1, "로또"),
     NUMBER_BASEBALL(Level.LEVEL1, "숫자야구게임"),
 
-    // LEVEL 2 Missions
     SHOPPING_CART(Level.LEVEL2, "장바구니"),
     PAYMENT(Level.LEVEL2, "결제"),
     SUBWAY_ROUTE(Level.LEVEL2, "지하철노선도"),
 
-    // LEVEL 4 Missions
     PERFORMANCE_IMPROVEMENT(Level.LEVEL4, "성능개선"),
     DEPLOYMENT(Level.LEVEL4, "배포");
 
