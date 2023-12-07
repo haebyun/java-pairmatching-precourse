d08f6735272c421db748e1e735b4b60a5a62c35e
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-07 16:01:51 +0900

    feat(domain): PairMatcher 클래스 클린코드 및 매치조회 함수 병합

--- src/main/java/pairmatching/domain/PairMatcher.java
+++ src/main/java/pairmatching/domain/PairMatcher.java
@@ -2,67 +2,55 @@ package pairmatching.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class PairMatcher {
-    private List<MatchingInfo> matchingResults = new ArrayList<>();
-    private CrewRepository crewRepository;
+    private final List<MatchingInfo> matchingResults = new ArrayList<>();
+    private final CrewRepository crewRepository;
 
     public PairMatcher(CrewRepository crewRepository) {
         this.crewRepository = crewRepository;
     }
 
     public boolean hasPreviousMatchingInfo(CourseLevelMissionInput courseLevelMissionInput) {
-        MatchingInfo existingMatchingInfo = findMatchingInfo(courseLevelMissionInput);
-
-        if (existingMatchingInfo == null) {
-            return false;
-        }
-        return true;
+        return findMatchingInfo(courseLevelMissionInput).isPresent();
     }
 
     public void matchPairs(CourseLevelMissionInput courseLevelMissionInput) {
-        MatchingInfo existingMatchingInfo = findMatchingInfo(courseLevelMissionInput);
+        Optional<MatchingInfo> existingMatchingInfo = findMatchingInfo(courseLevelMissionInput);
 
-        if (existingMatchingInfo == null) {
-            createMatchingInfo(courseLevelMissionInput);
+        if (existingMatchingInfo.isEmpty()) {
+            createNewMatchingInfo(courseLevelMissionInput);
             return;
         }
 
-        updateMatchingInfo(existingMatchingInfo);
+        updateExistingMatchingInfo(existingMatchingInfo.get());
     }
 
-    private MatchingInfo findMatchingInfo(CourseLevelMissionInput courseLevelMissionInput) {
-        for (MatchingInfo matchingInfo : matchingResults) {
-            if (matchingInfo.getCourse() == courseLevelMissionInput.getCourse() &&
-                    matchingInfo.getLevel() == courseLevelMissionInput.getLevel() &&
-                    matchingInfo.getMission() == courseLevelMissionInput.getMission()) {
-                return matchingInfo;
-            }
-        }
-        return null;
-    }
 
-    private void updateMatchingInfo(MatchingInfo matchingInfo) {
+    public Optional<MatchingInfo> findMatchingInfo(CourseLevelMissionInput courseLevelMissionInput) {
+        return matchingResults.stream()
+                .filter(matchingInfo -> matchingInfo.matchesCourseLevelMission(courseLevelMissionInput))
+                .findFirst();
+      }
+
+    private void updateExistingMatchingInfo(MatchingInfo matchingInfo) {
         List<Crew> selectedCrews = crewRepository.getCrewsByCourse(matchingInfo.getCourse());
         crewRepository.shuffleCrews(selectedCrews);
-        List<Pair> pairs = performPairMatching(crewRepository.getCrews());
-
-        matchingInfo.updatePairs(pairs);
+        matchingInfo.updatePairs(performPairMatching(selectedCrews));
     }
 
-    private void createMatchingInfo(CourseLevelMissionInput courseLevelMissionInput) {
+    private void createNewMatchingInfo(CourseLevelMissionInput courseLevelMissionInput) {
         List<Crew> selectedCrews = crewRepository.getCrewsByCourse(courseLevelMissionInput.getCourse());
         crewRepository.shuffleCrews(selectedCrews);
-        List<Pair> pairs = performPairMatching(crewRepository.getCrews());
-        MatchingInfo matchingInfo = new MatchingInfo(courseLevelMissionInput.getCourse(), courseLevelMissionInput.getLevel(), courseLevelMissionInput.getMission(), pairs);
-        matchingResults.add(matchingInfo);
+        MatchingInfo newMatchingInfo = new MatchingInfo(courseLevelMissionInput.getCourse(), courseLevelMissionInput.getLevel(), courseLevelMissionInput.getMission(), performPairMatching(selectedCrews));
+        matchingResults.add(newMatchingInfo);
     }
 
     public List<Pair> performPairMatching(List<Crew> crews) {
         List<Pair> pairs = new ArrayList<>();
         for (int i = 0; i < crews.size(); i += 2) {
             if (i == crews.size() - 1) {
-                // 마지막 남은 한 명인 경우, 이전 페어에 추가
                 pairs.get(pairs.size() - 1).addCrew(crews.get(i));
                 continue;
             }
@@ -71,16 +59,7 @@ public class PairMatcher {
         return pairs;
     }
 
-    public MatchingInfo getMatchingInfoByCourseLevelMission(Course course, Level level, Mission mission) {
-        for (MatchingInfo matchingInfo : matchingResults) {
-            if (matchingInfo.getCourse() == course && matchingInfo.getLevel() == level && matchingInfo.getMission() == mission) {
-                return matchingInfo;
-            }
-        }
-        return null;
-    }
-
     public void initializeMatchingResults() {
-        matchingResults = new ArrayList<>();
+        matchingResults.clear();
     }
 }
