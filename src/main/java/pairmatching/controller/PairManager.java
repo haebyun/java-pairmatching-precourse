7a83ea1fbe7800143aaba52deaef8c7e23cc961b
MingyeomKim <mingyum119@naver.com>
2023-12-14 22:53:25 +0900

    feat: 매칭 요청에 대해 페어 매칭 메서드를 호출

--- src/main/java/pairmatching/controller/PairManager.java
+++ src/main/java/pairmatching/controller/PairManager.java
@@ -1,9 +1,18 @@
 package pairmatching.controller;
 
 import java.util.List;
+import java.util.function.Supplier;
+import pairmatching.controller.dto.Stage;
+import pairmatching.domain.constants.GameCommand;
+import pairmatching.domain.constants.Level;
+import pairmatching.domain.constants.Mission;
+import pairmatching.global.exception.CustomException;
+import pairmatching.global.exception.ErrorMessage;
 import pairmatching.global.util.CustomFileReader;
+import pairmatching.service.PairMatcher;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
+import pairmatching.view.console.ConsoleWriter;
 
 public class PairManager {
     private final InputView inputView;
@@ -19,5 +28,41 @@ public class PairManager {
         this.pairMatcher = new PairMatcher(backend, frontend);
     }
 
+    public void run() {
+        while (true) {
+            GameCommand gameCommand = inputView.readFunction();
+            if (gameCommand.equals(GameCommand.QUIT)) {
+                break;
+            }
+            if (gameCommand.equals(GameCommand.MATCHING)) {
+                match();
+            }
+        }
+    }
 
+    private void match() {
+        outputView.printCourseAndMission();
+        Stage stage = retry(() -> {
+            Stage _stage = inputView.readStage();
+            if (doesNotContains(_stage.level(), _stage.mission())) {
+                throw CustomException.from(ErrorMessage.NOT_MATCHED_MISSION);
+            }
+            return _stage;
+        });
+        pairMatcher.execute(stage);
+    }
+
+    private boolean doesNotContains(Level level, Mission mission) {
+        return level.getMissions().contains(mission);
+    }
+
+    private static <T> T retry(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                ConsoleWriter.printlnMessage(e.getMessage());
+            }
+        }
+    }
 }
