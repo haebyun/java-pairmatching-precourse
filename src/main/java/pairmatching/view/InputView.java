cd72fe29c385248faec56427a451a6c11b744a6e
MingyeomKim <mingyum119@naver.com>
2023-12-14 22:41:04 +0900

    feat: 기능 목록을 출력하는 기능 구현

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -2,6 +2,7 @@ package pairmatching.view;
 
 import java.util.Arrays;
 import java.util.List;
+import pairmatching.domain.constants.GameCommand;
 import pairmatching.view.console.ConsoleReader;
 import pairmatching.view.console.ConsoleWriter;
 
@@ -9,6 +10,17 @@ public class InputView {
     private static final String READ_MISSION_NOTICE = "과정, 레벨, 미션을 선택하세요.";
     private static final String READ_MISSION_EXAMPLE = "ex) 백엔드, 레벨1, 자동차경주";
     private static final String SEPARATOR = ", ";
+
+    public GameCommand readFunction() {
+        ConsoleWriter.printlnMessage("기능을 선택하세요.");
+        ConsoleWriter.printlnMessage("1. 페어 매칭");
+        ConsoleWriter.printlnMessage("2. 페어 조회");
+        ConsoleWriter.printlnMessage("3. 페어 초기화");
+        ConsoleWriter.printlnMessage("Q. 종료");
+        return GameCommand.from(
+                ConsoleReader.enterMessage()
+        );
+    }
 
     public List<String> readMission() {
         ConsoleWriter.printlnMessage(READ_MISSION_NOTICE);
@@ -24,4 +36,6 @@ public class InputView {
     private static String[] split(String message, String separator) {
         return message.split(separator, -1);
     }
+
+
 }
