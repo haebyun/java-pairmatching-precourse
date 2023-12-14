069b0ab77146b4b60a1d3844f72da63634b762d8
MingyeomKim <mingyum119@naver.com>
2023-12-14 21:21:29 +0900

    feat: 과정, 레벨, 미션을 입력하는 기능 구현

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,32 @@
+package pairmatching.view;
+
+import java.util.Arrays;
+import java.util.List;
+import pairmatching.view.console.ConsoleReader;
+import pairmatching.view.console.ConsoleWriter;
+
+public class InputView {
+    private static final String READ_MISSION_NOTICE = "과정, 레벨, 미션을 선택하세요.";
+    private static final String READ_MISSION_EXAMPLE = "ex) 백엔드, 레벨1, 자동차경주";
+    private static final String SEPARATOR = ", ";
+
+    public List<String> readMission() {
+        printCourseAndMissions();
+        ConsoleWriter.printlnMessage(READ_MISSION_NOTICE);
+        ConsoleWriter.printlnMessage(READ_MISSION_EXAMPLE);
+        String message = ConsoleReader.enterMessage();
+        return parseStringToList(message, SEPARATOR);
+    }
+
+    private void printCourseAndMissions() {
+        
+    }
+
+    private static List<String> parseStringToList(String message, String separator) {
+        return Arrays.stream(split(message, separator)).toList();
+    }
+
+    private static String[] split(String message, String separator) {
+        return message.split(separator, -1);
+    }
+}
